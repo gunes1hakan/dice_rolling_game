d4f94ba8fc95916ab237bc0cf2516d4e5256dd9d
gunes1hakan <gunes1.hakan1@gmail.com>
2026-02-25 01:24:53 +0300

    file structures edited
    --State structures have been moved to a new file structure, improving code readability.
    
    --The test class has been updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,11 @@
+import 'package:dice_rolling_game/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.blue, Colors.red],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "hello world!",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
